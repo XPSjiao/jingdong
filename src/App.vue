0163dd18ae240750193eb8d6a465506b48345f64
EthanJiao <ethajiao@foxmail.com>
2022-04-25 16:19:51 +0800

    完成顶部导航栏 搜索区 banner栏

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <template>
+<!-- 顶部导航 -->
+<div class="wrapper">
+    <!-- 定位 -->
+    <div class="wrapper__position">
+      <span class="iconfont">&#xe64c;</span>
+      江苏省南京市栖霞区羊山北路1号
+      <span class="iconfont position__notice">&#xe60d;</span>
+    </div>
+
+    <!-- 搜索区域 -->
+    <div class="wrapper__search">
+      <span class="iconfont">&#xe741;</span>
+      <span class="search__test">山姆会员商店优惠商品</span>
+    </div>
+
+    <!-- banner -->
+    <div class="wrapper__banner">
+      <img
+        src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/banner/jingdong/banner1.jpg"
+        class="wrapper__banner__img"
+        alt=""
+      />
+    </div>
+    <!-- <div>123</div> -->
+
+</div>
+
   <!-- 底部导航 -->
   <div class="docker">
     <!-- BEM block__element--modifier -->
@@ -23,6 +50,54 @@
 
 <style lang="scss">
 @import "./style/index.scss";
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0.5rem;
+  padding: 0 0.18rem;
+  &__position {
+    position: relative;
+    padding: 0.16rem 0.16rem 0.16rem 0;
+    font-size: 0.16rem;
+    @include ellipse;
+    .iconfont {
+      font-size: 0.2rem;
+    }
+    // background: green;
+    .position__notice {
+      position: absolute;
+      right: 0;
+    }
+    color: $content-fontcolor;
+  }
+  &__search {
+    margin-bottom: 0.12rem;
+    line-height: 0.32rem;
+    border-radius: 0.16rem;
+    background-color: $background-color;
+    color: $placeholder-color;
+    .iconfont {
+      display: inline-block;
+      padding: 0 0.12rem 0 0.16rem;
+      font-style: 0.2rem;
+    }
+    &__text {
+      // display: inline-block;
+      font-size: 0.14rem;
+    }
+  }
+  &__banner {
+    height: 0;
+    overflow: hidden;
+    // hack技巧，预留高度防抖（图片高/图片宽）
+    padding-bottom: 25.4%;
+    &__img {
+      width: 100%;
+    }
+  }
+}
 .docker {
   display: flex;
   position: absolute;

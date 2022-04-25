4902e8de2d9fb43f133c25205b031362f29fe931
EthanJiao <ethajiao@foxmail.com>
2022-04-25 20:28:27 +0800

    完成 京东到家的首页(未抽组件版本)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-<!-- 顶部导航 -->
-<div class="wrapper">
+  <!-- 顶部导航 -->
+  <div class="wrapper">
     <!-- 定位 -->
     <div class="wrapper__position">
       <span class="iconfont">&#xe64c;</span>
@@ -17,90 +17,254 @@
     <!-- banner -->
     <div class="wrapper__banner">
       <img
-        src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/banner/jingdong/banner1.jpg"
+        src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/banner1.jpg"
         class="wrapper__banner__img"
         alt=""
       />
     </div>
     <!-- <div>123</div> -->
-     <!-- icon宫格区 -->
-     <div class="icons">
+    <!-- icon宫格区 -->
+    <div class="icons">
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E8%B6%85%E5%B8%82.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">超市便利</p>
+        <p class="icons__item__desc">超市便利</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E8%8F%9C%E5%B8%82%E5%9C%BA.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">菜市场</p>
+        <p class="icons__item__desc">菜市场</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E6%B0%B4%E6%9E%9C%E5%BA%97.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">水果店</p>
+        <p class="icons__item__desc">水果店</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E9%B2%9C%E8%8A%B1.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">鲜花绿植</p>
+        <p class="icons__item__desc">鲜花绿植</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E5%8C%BB%E8%8D%AF%E5%81%A5%E5%BA%B7.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">医药健康</p>
+        <p class="icons__item__desc">医药健康</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E5%AE%B6%E5%B1%85.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">家居时间</p>
+        <p class="icons__item__desc">家居时间</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E8%9B%8B%E7%B3%95.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">烘培蛋糕</p>
+        <p class="icons__item__desc">烘培蛋糕</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E7%AD%BE%E5%88%B0.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">签到</p>
+        <p class="icons__item__desc">签到</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E5%A4%A7%E7%89%8C%E5%85%8D%E8%BF%90.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">大牌搬运</p>
+        <p class="icons__item__desc">大牌搬运</p>
       </div>
       <div class="icons__item">
         <img
           src="https://ossstored.oss-cn-shanghai.aliyuncs.com/img/jingdong/%E7%BA%A2%E5%8C%85.png"
           class="icons__item__img"
         />
-        <p  class="icons__item__desc">红包套餐</p>
+        <p class="icons__item__desc">红包套餐</p>
       </div>
     </div>
 
     <!-- gap 分割条 -->
     <div class="gap"></div>
 
-</div>
+    <!-- 附近店铺 -->
+    <div class="nearby">
+      <h3 class="nearby__title">附近店铺</h3>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop1.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop2.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">联合利华洗护满10减5</div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop1.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop2.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">联合利华洗护满10减5</div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop1.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop2.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">联合利华洗护满10减5</div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop1.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop2.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">联合利华洗护满10减5</div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop1.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://soft2176-use.oss-cn-hangzhou.aliyuncs.com/jingdong/icon/shop2.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby_content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送￥0元</span>
+            <span class="nearby__content__tag">基础运费￥5</span>
+          </div>
+          <div class="nearby__content__highlight">联合利华洗护满10减5</div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- 底部导航 -->
   <div class="docker">
@@ -127,6 +291,7 @@
 <style lang="scss">
 @import "./style/index.scss";
 .wrapper {
+  overflow-y: auto;
   position: absolute;
   left: 0;
   right: 0;
@@ -195,9 +360,52 @@
 }
 
 .gap {
-  margin: 0 -0.18rem ;
+  margin: 0 -0.18rem;
   height: 0.1rem;
   background-color: #f1f1f1;
+}
+
+.nearby {
+  &__title {
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+  &__item {
+    display: flex;
+    padding-top: 0.12rem;
+    &__img {
+      margin-right: 0.16rem;
+      width: 0.56rem;
+      height: 0.56rem;
+    }
+  }
+  &__content {
+    flex: 1;
+    padding-bottom: 0.12rem;
+    border-bottom: 1px solid $border-color;
+    &__title {
+      line-height: 0.22rem;
+      font-size: 0.16rem;
+      color: $content-fontcolor;
+    }
+    &__tags {
+      margin-top: 0.08rem;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: $content-fontcolor;
+      &__tag {
+        margin-right: 0.16rem;
+      }
+    }
+    &__highlight {
+      margin: 0.08rem 0 0 0;
+      line-height: 0.18rem;
+      font-size: 0.13rem;
+      color: #e93b3b;
+    }
+  }
 }
 .docker {
   display: flex;
